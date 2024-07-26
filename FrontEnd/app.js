23a85da1baaf4a78465227ef347a72670c108203
akansh-agarwalalwar <akanshagarwal.alwar@gmail.com>
2024-07-26 14:11:07 +0530

    Project

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -4,7 +4,13 @@ import { Provider } from "react-redux";
 import appStore from "./src/store/appStore";
 import AppRouter from "./appRouter";
 import "./globalStyles.css";
-
+import './App.css'
+function disableRightClick() {
+    document.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+    }, false);
+  }
+  disableRightClick();
 const App = () => {
     return (
         <Provider store={appStore}>
